91ba82b8cf94ac330692957b470fcc2cdf9f86e8
cariuss <guillentcarlos@gmail.com>
2024-02-19 16:34:06 -0500

    ya va

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -3,7 +3,7 @@ import React from "react";
 export const Button = ({ label, fn }) => {
   return (
     <>
-        <button onClick={fn}>{label}</button> 
+        <button onClick={fn}>{label}</button>     
     </>
   );
 };
